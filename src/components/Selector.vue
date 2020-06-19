f42feb99b803049aeb4555e3916a44beec369805
codepanov@gmail.com <codepanov@gmail.com>
2020-06-20 01:05:00 +0200

    finishing main component logic

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -4,20 +4,36 @@
       {{ showSelected }}
       <img src="@/assets/icn-arrow-down.svg" alt="Arrow" ref="image">
     </div>
-    <div class="s-modal" :class="{'s-modal-expanded': expanded}">
+    <div class="s-modal" :class="{'s-modal-expanded': expanded}" ref="modal">
       <div class="s-search">
-        <input type="text" placeholder="Search" v-model="search">
-        <img src="@/assets/icn-close.svg" alt="Close" ref="close" @click="reset_search">
+        <input type="text" placeholder="Search for a user..." v-model="search">
+        <img 
+          src="@/assets/search.svg" 
+          alt="Search" 
+          ref="search" 
+          class="icon-search"
+        >
+        <img 
+          src="@/assets/icn-close.svg" 
+          alt="Close" 
+          ref="close" 
+          @click="reset_search" 
+          v-show="search != ''" 
+          class="icon-close"
+        >
       </div>
       <div class="s-list">
-        <input type="checkbox" @click="selectAll" v-model="allSelected">
-        <label>Select all</label>
+        <div v-show="search == ''">
+          <input type="checkbox" @click="selectAll" v-model="allSelected">
+          <label>Select all</label>
+        </div>
         <ul>
           <li v-for="(user, i) in searchedUsers" :key="i">
-            <input type="checkbox" :value="user.name" v-model="selectedUsers">
-            <label :for="user.name">{{ user.name }}</label>
+            <input type="checkbox" :id="user" :value="user" v-model="selectedUsers">
+            <label :for="user">{{ user }}</label>
           </li>
         </ul>
+        <span v-show="!searchedUsers.length">No Results Found</span>
       </div>
     </div>
   </div>
@@ -28,10 +44,26 @@ export default {
   data() {
     return {
       users: [
-        {name: 'Patrick'},
-        {name: 'Evan'},
-        {name: 'Christian'},
-        {name: 'Daniel'}
+        'zena.huels@gmail.com',
+        'welch.lauren@gmail.com',
+        'tillman.marisa@cole.com',
+        'mariela.lesch@gmail.com',
+        'abshire.bertrand@hotmail.com',
+        'bledner@hotmail.com',
+        'skreiger@jacobs.biz',
+        'dbrakus@yahoo.com',
+        'tyra.heidenreich@powlowski.com',
+        'vwehner@tremblay.com',
+        'kessler.eugene@gmail.com',
+        'murray.spencer@wisozk.info',
+        'toby63@welch.com',
+        'rashawn51@yahoo.com',
+        'kelsi.crona@crona.net',
+        'keebler.monty@mann.com',
+        'blanda.rhett@gaylord.com',
+        'lebsack.kristy@hotmail.com',
+        'bridget33@schuster.com',
+        'herzog.maymie@wehner.com'
       ],
       showSelected: 'No user selected',
       expanded: false,
@@ -42,7 +74,10 @@ export default {
   },
   methods: {
     expand() {
-      this.$refs.selected.addEventListener('mousedown', function(e){ e.preventDefault(); }, false)
+      // preventing text and img selection
+      let refs = [this.$refs.selected, this.$refs.modal] // putting refs in array and mapping to avoid redundacy
+      refs.map(ref => ref.addEventListener('mousedown', function(e){ e.preventDefault(); }, false))
+      
       if(!this.expanded)
         this.$refs.image.setAttribute('style', `transform: rotateZ(180deg)`)
       else
@@ -53,7 +88,7 @@ export default {
       this.selectedUsers = []
       this.allSelected = !this.allSelected
       if(this.allSelected )
-        this.users.map(user => this.selectedUsers.push(user.name))
+        this.users.map(user => this.selectedUsers.push(user))
     },
     reset_search() {
       this.search = ''
@@ -70,12 +105,15 @@ export default {
       }
       this.allSelected = false
       console.log(this.selectedUsers)
+    },
+    search() {
+      console.log(this.searchedUsers)
     }
   },
   computed: {
     searchedUsers() {
       return this.users.filter(user => {
-        return user.name
+        return user
         .toLowerCase()
         .includes(this.search.toLowerCase())
       })
@@ -91,11 +129,12 @@ export default {
   justify-content: space-between;
   border: 1px solid red;
   border-radius: 3px;
-  padding: 12px;
+  padding: 10px;
   width: 265px;
   background: white;
   position: relative;
   z-index: 2;
+  cursor: pointer;
 }
 .s-selected img {
   transition: transform 0.25s ease-in-out;
@@ -103,8 +142,8 @@ export default {
 .s-modal {
   border: 1px solid blue;
   border-radius: 3px;
-  padding: 12px;
-  width: 265px;
+  padding: 10px;
+  min-width: 265px;
   margin-top: 5px;
   opacity: 0;
   position: absolute;
@@ -112,6 +151,8 @@ export default {
   transition: all 0.5s;
   z-index: -1;
   background: white;
+  width: fit-content;
+  overflow-y: scroll;
 }
 .s-modal-expanded {
   opacity: 1;
@@ -122,5 +163,35 @@ export default {
   list-style-type: none;
   margin: 0;
   padding: 0;
+}
+.s-search {
+  position:relative;
+  display: flex;
+  align-items: center;
+}
+.s-search input{
+  padding: 10px;
+  padding-left: 35px;
+  border-radius: 5px;
+  width: 100%;
+}
+.icon-search {
+  position: absolute;
+  left: 10px;
+}
+.icon-close {
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+}
+.s-list {
+  margin-top: 12px;
+  height: 300px;
+}
+.s-list label {
+  cursor: pointer;
+}
+.s-list label:hover {
+  color: red;
 }
 </style>
